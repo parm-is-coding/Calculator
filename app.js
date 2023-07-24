29d33263fa50d1788b43534ae84021d54a5b0d0b
parm-is-coding <pardhi1400@gmail.com>
2023-07-24 10:22:13 -0700

    create and test operate fn

--- app.js
+++ app.js
@@ -10,9 +10,29 @@ function multiply(num1,num2){
 function divide(num1,num2){
     return num1/num2;
 }
+function operate(num1,opp,num2){
+    if(opp === "+"){
+        return add(num1,num2);
+    }else if(opp === "-"){
+        return subtract(num1,num2);
+    }else if (opp === "*"){
+        return multiply(num1,num2);
+    }else if (opp === "/"){
+        return divide(num1,num2);
+    }else{
+        console.log("Invalid Input");
+    }
+} 
+
 let num1 = 5;
-let num2 = 2; 
-console.log(add(num1,num2));
-console.log(subtract(num1,num2));
-console.log(multiply(num1,num2));
-console.log(divide(num1,num2));
+let opp1 = "+";
+let opp2 = "-";
+let opp3 = "*";
+let opp4 = "/";
+let num2 = 2;
+
+console.log(operate(num1,opp1,num2));
+console.log(operate(num1,opp2,num2));
+console.log(operate(num1,opp3,num2));
+console.log(operate(num1,opp4,num2));
+
