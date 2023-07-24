72abdbd4d26a139dc529d83dfc2295847b5ef78e
parm-is-coding <pardhi1400@gmail.com>
2023-07-24 10:12:25 -0700

    create add subtract multiply divide fn

---
+++ app.js
@@ -0,0 +1,18 @@
+function add (num1, num2){
+    return num1 + num2;
+}
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function multiply(num1,num2){
+    return num1 * num2;
+}
+function divide(num1,num2){
+    return num1/num2;
+}
+let num1 = 5;
+let num2 = 2; 
+console.log(add(num1,num2));
+console.log(subtract(num1,num2));
+console.log(multiply(num1,num2));
+console.log(divide(num1,num2));
