3f3e6bae1dbbe7a2cf80238c308067099227cb80
parm-is-coding <pardhi1400@gmail.com>
2023-07-26 11:50:46 -0700

    add buttonContainer refactor createButton

--- app.js
+++ app.js
@@ -32,19 +32,19 @@ function createButton(content){
 
 function createButtons(){
     for (let i = 9; i >=0; i--){
-        container.appendChild(createButton(i));
+        buttonContainer.appendChild(createButton(i));
         if(i == 7){
             let button = createButton("+");
             button.setAttribute('id','add');
-            container.appendChild(button);
+            buttonContainer.appendChild(button);
         }else if(i == 4){
             let button = createButton("-");
             button.setAttribute('id','subtract');
-            container.appendChild(button);
+            buttonContainer.appendChild(button);
         }else if(i == 1){
             let button = createButton("*");
             button.setAttribute('id','multiply');
-            container.appendChild(button);
+            buttonContainer.appendChild(button);
         }
     }
     const dot = createButton(".");
@@ -53,9 +53,9 @@ function createButtons(){
     dot.setAttribute('id',"dot");
     equal.setAttribute('id',"equal");
     divide.setAttribute('id', "divide");
-    container.appendChild(dot);
-    container.appendChild(equal);
-    container.appendChild(divide);
+    buttonContainer.appendChild(dot);
+    buttonContainer.appendChild(equal);
+    buttonContainer.appendChild(divide);
    
     
 }
@@ -73,7 +73,12 @@ console.log(operate(num1,opp3,num2));
 console.log(operate(num1,opp4,num2));
 
 const container = document.querySelector("#container");
+// add screen to calculator
 const screen = document.createElement("div");
 screen.setAttribute('id',"screen");
 container.appendChild(screen);
+// add buttonContainer to Calculator
+const buttonContainer = document.createElement('div');
+buttonContainer.setAttribute('id','buttonContainer');
 createButtons();
+container.appendChild(buttonContainer);
